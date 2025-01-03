d60f3c26af61667e8fe6da31949506b09e0c24ad
Ilya Semenenko <159060640+IlyaSemenenkoo@users.noreply.github.com>
2025-01-03 13:44:37 +0200

    Update Projectile.cs

--- Assets/Undead Survivor/Scripts/Services/Projectile.cs
+++ Assets/Undead Survivor/Scripts/Services/Projectile.cs
@@ -6,12 +6,44 @@ public class Projectile : NetworkBehaviour
 {
     public float Damage { get; set; }
     private float _lifeTime;
-    [SerializeField] private NetworkRigidbody2D _networkRB;
+    [SerializeField] private NetworkTransform _networkTransform;
     private Vector2 _direction;
+    private int _damage;
 
-    public void Initialize(Vector2 direction)
+    public void Initialize(Vector2 direction, float lifeTime, int damage)
     {
+        _lifeTime = lifeTime;
         _direction = direction;
+        _damage = damage;
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (HasStateAuthority)
+        {
+            if (_lifeTime > 0)
+            {
+                _networkTransform.transform.position += new Vector3(_direction.x, _direction.y, 0) * Runner.DeltaTime;
+            }
+            else DeleteBullet();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (HasStateAuthority)
+        {
+            if (collision.gameObject.TryGetComponent<HealthManager>(out var healthManager))
+            {
+                healthManager.SubtractHP(_damage);
+                DeleteBullet();
+            }
+        }
+    }
+
+    private void DeleteBullet()
+    {
+        Runner.Despawn(Object);
     }
 }
     
